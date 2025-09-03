5026442995a93886946ad3fa194ebee722f1cfdf
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-09-03 12:50:24 +0530

    practice

---
+++ Week3_PA1.py
@@ -0,0 +1,13 @@
+words = input("enter list of words separated by commas: ")
+words = words.split(",")
+print(words)
+maxlength = 0
+word =""
+
+for item in words:
+    length = len(item)
+    if(length>=maxlength):
+        maxlength = length
+        word = item
+print(word)
+    
